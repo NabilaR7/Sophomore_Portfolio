6fe26588f40c89c08b49414950228c1cbbe1c8ba
Nabila Raisa <144722332+NabilaR7@users.noreply.github.com>
2024-06-06 00:30:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 let intro = document.querySelector(',intro');
 let logo = ducument.querySelector('.logo-header');
-let logoSpan = document.querySelectorAll('.logo')
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded',()=>{
+
+  setTimeout(()=>{
+
+    logoSpan.forEach((span, idx)=>{
+      setTimeout(()=>{
+        span.classList.add('active');
+      }, (idx + 1) * 400)
+    });
+
+    SetTimeout(()=>{
+      logoSpan.forEach((span, idx)=>{
+        setTimeout(()=>{
+          span.classList.remove('active');
+          span.classList.add('fade');
+        })
+      })
+    })
+    
+  })
+})
+
+
+
+
+
+
+
+
+
+
 
 
 
